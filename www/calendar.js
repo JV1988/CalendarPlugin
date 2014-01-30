a814a698a949b4af9fade45aebcc32d966b6c8a2
JV1988 <jeroen@periskal.com>
2014-01-30 08:31:52 +0100

    Update calendar.js

--- www/calendar.js
+++ www/calendar.js
@@ -1,9 +1,9 @@
-var calendar =  {
+var calendar = {
     createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'Calendar', // mapped to our native Java class called "Calendar"
+            'Calendar', // mapped to our native Java class called "CalendarPlugin"
             'addCalendarEntry', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "title": title,
@@ -12,7 +12,7 @@ var calendar =  {
                 "startTimeMillis": startDate.getTime(),
                 "endTimeMillis": endDate.getTime()
             }]
-        );
+        ); 
     }
 }
 module.exports = calendar;
